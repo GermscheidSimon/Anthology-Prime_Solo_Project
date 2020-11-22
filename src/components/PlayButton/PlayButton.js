f1826dea7800d335ad85fe4227d5d5e688a9cefb
Khaildoran <sgermscheid@gmail.com>
2020-11-21 23:07:06 -0600

    moving Play button to component for material UI

---
+++ src/components/PlayButton/PlayButton.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+
+// Basic functional component structure for React with default state
+// value setup. When making a new component be sure to replace the
+// component name TemplateFunction with the name for the new component.
+function PlayButton(props) {
+  // Using hooks we're creating local state for a "heading" variable with
+  // a default value of 'Functional Component'
+
+  return (
+    <PlayArrowIcon   ></PlayArrowIcon>
+  );
+}
+
+export default connect(mapStoreToProps)(PlayButton);
